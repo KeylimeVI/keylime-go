5986e898852b3b9fd83b81fb6167f1bd522000f7
KeylimeVI <noa.higuchi@mail.utoronto.ca>
2025-10-04 23:10:58 -0400

    Added Pointer constructors for List and Set

--- essentials/essentials.go
+++ essentials/essentials.go
@@ -1,9 +1,8 @@
 package essentials
 
 import (
-	kl "github.com/KeylimeVI/keylime-go/list"
-	kp "github.com/KeylimeVI/keylime-go/pair"
-	ks "github.com/KeylimeVI/keylime-go/set"
+	"github.com/KeylimeVI/keylime-go/list"
+	"github.com/KeylimeVI/keylime-go/set"
 )
 
 type List[T any] = kl.List[T]
@@ -16,10 +15,4 @@ type Set[T comparable] = ks.Set[T]
 
 func NewSet[T comparable](items ...T) Set[T] {
 	return ks.NewSet(items...)
-}
-
-type Pair[A any, B any] = kp.Pair[A, B]
-
-func NewPair[A any, B any](a A, b B) Pair[A, B] {
-	return kp.NewPair(a, b)
 }
